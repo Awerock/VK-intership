a6e597cb731a09aae01b30d1d1d5412b80edab04
Awerock <107041868+Awerock@users.noreply.github.com>
2023-05-09 23:43:43 +0300

    init: add first file

---
+++ app.js
@@ -0,0 +1,13 @@
+new AirDatepicker('#airdatepicker');
+
+const section = document.createElement('section');
+section.className = intership;
+
+const container = document.createElement('div');
+container.className = container;
+
+const form = document.createElement('div');
+form.className = intership__form;
+
+const form = document.createElement('div');
+form.className = intership__form;
