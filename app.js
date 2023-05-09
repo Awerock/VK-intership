c09d2273348a730ea3b907534362b0f78fcba9e3
Awerock <cigan-yashka@yandex.ru>
2023-05-10 01:50:36 +0300

    add: 1 option

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 new AirDatepicker('#airdatepicker');
 
 const section = document.createElement('section');
-section.className = intership;
+section.classList.add("intership");
+document.body.appendChild(section);
 
 const container = document.createElement('div');
-container.className = container;
+container.classList.add("container");
+section.appendChild(container);
 
-const form = document.createElement('div');
-form.className = intership__form;
+const form = document.createElement('form');
+form.classList.add("intership__form");
+container.appendChild(form);
 
-const form = document.createElement('div');
-form.className = intership__form;
+const label = document.createElement('label');
+label.classList.add("intership__lable");
+label.textContent = "Выберите башню:";
+form.appendChild(label);
+
+const select = document.createElement('select');
+select.classList.add("intership__select");
+form.appendChild(select);
+
+const options = ['Башня А', 'Башня Б'];
+for (let i = 0; i < options.length; i++) {
+    const option = document.createElement('option');
+    option.textContent = options[i];
+    select.appendChild(option);
+  }
